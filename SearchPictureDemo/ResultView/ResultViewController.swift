5f1f91b29bf030e2600d746cd58a7e1d0e665961
Gordan_Feng <gordon_feng@17life.com>
2020-11-11 09:53:31 +0800

    add common

--- SearchPictureDemo/ResultView/ResultViewController.swift
+++ SearchPictureDemo/ResultView/ResultViewController.swift
@@ -44,7 +44,12 @@ class ResultViewController: UIViewController {
         super.viewWillAppear(animated)
         self.getData()
     }
-
+    
+    //TODO: 下拉重新打API更新
+    //TODO: 滑到最底時，秀出下一頁
+    //https://franksios.medium.com/ios-uitableview-%E5%88%86%E9%A0%81-%E6%BB%91%E8%87%B3%E5%88%97%E8%A1%A8%E6%9C%80%E5%BE%8C%E4%B8%80%E7%AD%86%E4%B8%8A%E6%8B%89%E8%BC%89%E5%85%A5%E6%9B%B4%E5%A4%9A%E8%B3%87%E6%96%99-loading-more-69ee25eae045
+    //TODO: 與我的最愛共用此頁
+    //TODO: getdata只跟data拿dataArray的資料
     func getData() {
         guard let searchInfo = self.searchInfo else { return }
         self.viewModel?.getData(text: searchInfo.text, perPage: searchInfo.perPage, page: searchInfo.page, completion: {
