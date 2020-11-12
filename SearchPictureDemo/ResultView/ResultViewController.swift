ce310901ecc762960cb0e4aa5eb15c0a9c1727bd
Gordan_Feng <gordon_feng@17life.com>
2020-11-12 09:32:50 +0800

    add pull to Refresh

--- SearchPictureDemo/ResultView/ResultViewController.swift
+++ SearchPictureDemo/ResultView/ResultViewController.swift
@@ -38,6 +38,7 @@ class ResultViewController: UIViewController {
         self.collectionView.dataSource = self
 
         self.setCollectionViewLayout()
+        self.setRefreshControl()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -49,6 +50,18 @@ class ResultViewController: UIViewController {
     //TODO: 滑到最底時，秀出下一頁
     //https://franksios.medium.com/ios-uitableview-%E5%88%86%E9%A0%81-%E6%BB%91%E8%87%B3%E5%88%97%E8%A1%A8%E6%9C%80%E5%BE%8C%E4%B8%80%E7%AD%86%E4%B8%8A%E6%8B%89%E8%BC%89%E5%85%A5%E6%9B%B4%E5%A4%9A%E8%B3%87%E6%96%99-loading-more-69ee25eae045
     //TODO: 與我的最愛共用此頁
+    
+    func setRefreshControl() {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(doRefreshthing), for: .valueChanged)
+        self.collectionView.refreshControl = refreshControl
+    }
+    
+    @objc func doRefreshthing() {
+        print("refreshing")
+        self.collectionView.refreshControl?.endRefreshing()
+    }
+
     //TODO: getdata只跟data拿dataArray的資料
     func getData() {
         guard let searchInfo = self.searchInfo else { return }
