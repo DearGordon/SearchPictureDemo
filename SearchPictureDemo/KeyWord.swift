7700a173788e61757af43436f2602f56101f1530
GordonFeng <hellogordon.f@gmail.com>
2020-11-15 19:16:13 +0800

    add UserDefault Keyword

--- SearchPictureDemo/KeyWord.swift
+++ SearchPictureDemo/KeyWord.swift
@@ -6,7 +6,33 @@
 //  Copyright © 2020 GordonFeng. All rights reserved.
 //
 
+import UIKit
+
 enum ResultViewKey {
     static var cellId = "ResultCell"
     static var viewControllerId = "ResultViewController"
+}
+
+extension UserDefaults {
+    struct CustomKey: RawRepresentable {
+        typealias RawValue = String
+
+        var rawValue: String
+
+        init(rawValue: String) {
+            self.rawValue = rawValue
+        }
+    }
+
+    func read(key: CustomKey) -> Any? {
+        self.value(forKey: key.rawValue)
+    }
+
+    func save(key: CustomKey, value: Any) {
+        self.setValue(value, forKey: key.rawValue)
+    }
+}
+
+extension UserDefaults.CustomKey {
+    static var favorited = UserDefaults.CustomKey(rawValue: "Favorited")
 }
